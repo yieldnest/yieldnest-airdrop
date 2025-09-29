4e1c8152cf908291ecd6f322fcca6500c311cb0e
danoctavian <danoctavian91@gmail.com>
2025-09-29 15:28:49 +0300

    fix BaseData so it refers to security council instead

--- script/BaseData.s.sol
+++ script/BaseData.s.sol
@@ -20,17 +20,20 @@ contract BaseData is Script {
 
     ChainIds public chainIds = ChainIds({ mainnet: 1, holesky: 17_000, anvil: 31_337, base: 8453 });
 
-    address public constant YN_DEV_MAINNET = 0xa08F39d30dc865CC11a49b6e5cBd27630D6141C3;
-    address public constant YN_DEV_HOLESKY = 0x72fdBD51085bDa5eEEd3b55D1a46E2e92f0837a5;
+    address public constant YN_SECURITYCOUNCIL_MAINNET = 0xfcad670592a3b24869C0b51a6c6FDED4F95D6975;
+    address public constant YN_SECURITYCOUNCIL_HOLESKY = 0x72fdBD51085bDa5eEEd3b55D1a46E2e92f0837a5;
 
     // test only
-    address public constant YN_DEV_TEST_BASE = 0x0d4ae80207c30E5489976d62661882A995b52155;
+    address public constant YN_SECURITYCOUNCIL_TEST_BASE = 0x0d4ae80207c30E5489976d62661882A995b52155;
 
     function setUp() public virtual {
-        __data[chainIds.mainnet] = Data({ airdropOwner: YN_DEV_MAINNET, proxyAdminOwner: YN_DEV_MAINNET });
+        __data[chainIds.mainnet] =
+            Data({ airdropOwner: YN_SECURITYCOUNCIL_MAINNET, proxyAdminOwner: YN_SECURITYCOUNCIL_MAINNET });
 
-        __data[chainIds.holesky] = Data({ airdropOwner: YN_DEV_HOLESKY, proxyAdminOwner: YN_DEV_HOLESKY });
-        __data[chainIds.base] = Data({ airdropOwner: YN_DEV_TEST_BASE, proxyAdminOwner: YN_DEV_TEST_BASE });
+        __data[chainIds.holesky] =
+            Data({ airdropOwner: YN_SECURITYCOUNCIL_HOLESKY, proxyAdminOwner: YN_SECURITYCOUNCIL_HOLESKY });
+        __data[chainIds.base] =
+            Data({ airdropOwner: YN_SECURITYCOUNCIL_TEST_BASE, proxyAdminOwner: YN_SECURITYCOUNCIL_TEST_BASE });
     }
 
     function getData(uint256 chainId) internal view returns (Data memory) {
